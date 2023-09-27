b50df0425f9d193bf7ce85b0c6f12448b1539f1f
@sridattaraghavendra <dattu046@tamu.edu>
2023-09-26 22:41:36 -0500

    MP3 #comment map first 4MB of shared space and enable paging

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -16,26 +16,57 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
-   Console::puts("Initialized Paging System\n");
+  kernel_mem_pool = _kernel_mem_pool;
+  process_mem_pool = _process_mem_pool;
+  shared_size = _shared_size;
 }
 
 PageTable::PageTable()
 {
-   assert(false);
-   Console::puts("Constructed Page Table object\n");
+   /*Get the page directory page from the kernel frame pool*/
+   unsigned long page_directory_number = kernel_mem_pool->get_frames(1);
+   page_directory = (unsigned long *)(page_directory_number * PAGE_SIZE);
+
+   /*Get one more page table from kernel frame pool for mapping the
+          Direct memory till 4MB*/
+   /*To map the first 4MB we just need to have 1024 entries since there are 1024 pages till 4MB*/
+   unsigned long page_table_number = kernel_mem_pool->get_frames(1);
+   unsigned long *page_table = (unsigned long *)(page_table_number * PAGE_SIZE);
+
+   unsigned int page_entry_valid_status = 3;
+   unsigned int page_entry_not_valid_status = 2;
+
+   /*Assign the address of the page table page to the first page directory entry*/
+   page_directory[0] = (unsigned long)page_table | page_entry_valid_status;
+
+   /*Mark rest of the page directory entries as invalid*/
+   for(int page_directory_index = 1; page_directory_index < 1024;page_directory_index++){
+      page_directory[page_directory_index] |= page_entry_not_valid_status;
+   }
+
+   /*Start address is 0 and we increment this address by the page size which is 4K*/
+   unsigned long page_address = 0;
+
+   /*Initialize the 1024 frame addresses in the page table page*/
+   for(int page_table_index = 0; page_table_index < shared_size / PAGE_SIZE; page_table_index++){
+     page_table[page_table_index] = page_address | page_entry_valid_status;
+     page_address = page_address + PAGE_SIZE;
+   }
+
+   Console::puts("Page table setup successfully.\n");
 }
 
 
 void PageTable::load()
 {
-   assert(false);
+   current_page_table = this;
+   write_cr3((unsigned long)page_directory);
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
+   write_cr0(read_cr0() | 0x1 << 31);
    Console::puts("Enabled paging\n");
 }
 
