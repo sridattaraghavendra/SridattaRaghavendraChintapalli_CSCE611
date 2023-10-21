0d4a51b39ea6f9a7dbe8f6eef6823cb9851a2572
@sridattaraghavendra <dattu046@tamu.edu>
2023-10-21 14:46:50 -0500

    MP4 #comment submission related changes

--- MP4/copykernel.sh
+++ MP4/copykernel.sh
@@ -1,5 +1,5 @@
 # Replace "/mnt/floppy" with the whatever directory is appropriate.
-sudo mount -o loop dev_kernel_grub.img /mnt/floppy
+sudo mount -o loop mp4.img /mnt/floppy
 sudo cp kernel.bin /mnt/floppy
 sleep 1s
 sudo umount /mnt/floppy
