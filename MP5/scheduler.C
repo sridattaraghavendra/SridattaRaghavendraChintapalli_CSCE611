3b36b4c3eb5dd1a9e968a03782686a1149795df3
@sridattaraghavendra <dattu046@tamu.edu>
2023-10-30 16:27:16 -0500

    MP5 #comment simple FIFO queue implementation in scheduler

--- MP5/scheduler.C
+++ MP5/scheduler.C
@@ -46,7 +46,11 @@
 /*--------------------------------------------------------------------------*/
 
 Scheduler::Scheduler() {
-  assert(false);
+  /*Initialize the queue objects*/
+  queue = new Thread*[MAX_QUEUE_SIZE];
+  head = -1;
+  tail = -1;
+  
   Console::puts("Constructed Scheduler.\n");
 }
 
@@ -59,9 +63,50 @@ void Scheduler::resume(Thread * _thread) {
 }
 
 void Scheduler::add(Thread * _thread) {
-  assert(false);
+  enqueue(_thread);
 }
 
 void Scheduler::terminate(Thread * _thread) {
   assert(false);
+}
+
+/*Scheduler queue functions*/
+void Scheduler::enqueue(Thread * _thread){
+  if(isQueueFull()){
+    Console::puts("Queue is full, cannot accept any more threads.\n");
+    assert(false);
+  }
+
+  if(isQueueEmpty()){
+    head = 0;
+  }
+
+  tail++;
+  queue[tail] = _thread;
+}
+
+Thread * Scheduler::dequeue(){
+  if(isQueueEmpty()){
+    Console::puts("Queue is empty. No threads available to execute\n");
+    assert(false);
+  }
+
+  Thread * thread = queue[head];
+
+  if(head == tail){
+    head = -1;
+    tail = -1;
+  }else{
+    head++;
+  }
+
+  return thread;
+}
+
+bool Scheduler::isQueueEmpty(){
+  return head == -1;
+}
+
+bool Scheduler::isQueueFull(){
+  return tail == MAX_QUEUE_SIZE - 1;
 }
